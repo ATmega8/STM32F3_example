23b208b42b88e3d1669299561563de6f169ad436
liu yang <295017359@qq.com>
2014-03-02 09:19:33 +0000

    change

--- F373/QP_TEST/bsp.c
+++ F373/QP_TEST/bsp.c
@@ -18,7 +18,7 @@ BSP_init(void)
 	GPIO_InitStructure.GPIO_Speed 	= GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	USART_InitStructure.USART_BaudRate   = 115200;
+	USART_InitStructure.USART_BaudRate   = 460800;
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
 	USART_InitStructure.USART_StopBits   = USART_StopBits_1;
 	USART_InitStructure.USART_Parity	 = USART_Parity_No;
